cb78dee4e4af464b2ab51060001256c21c5e48c9
Michelle Caraballo <caraballomp@yahoo.com>
2022-04-11 11:06:19 -0700

    file

---
+++ .github/workflows/song.rb
@@ -0,0 +1,11 @@
+class Song 
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
+
+hotline_bling = Song.new("Hotline Bling")
+hotline_bling = Song.new("THriller")
